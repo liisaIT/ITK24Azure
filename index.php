0d57640d07acf94a425a96e2f3b18167f7533d84
liisaIT <liisamanglus@gmail.com>
2025-03-14 14:52:37 +0200

    Update index.php
    Greetings and some content

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
     <title>
       ITK24 Azure
     </title>
-    
+    <body>
+      <h1>
+        Meie projekt ITK24 grupiga
+      </h1>
+      <p> See leht arvestab meie muutustega (ilgelt dünaamiline) </p>
+    </body>
   </head>
 </html>
